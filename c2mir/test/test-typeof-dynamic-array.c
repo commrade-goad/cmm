e252b3acc0d0685cb0a093b74eb914f55e9dbecd
Fernando Perry <fernandoperry1234@gmail.com>
2026-07-07 11:55:14 +0700

    dynamic array on stdlib

--- c2mir/test/test-typeof-dynamic-array.c
+++ c2mir/test/test-typeof-dynamic-array.c
@@ -1,55 +1,23 @@
 #include ":c2m_stdlib.hmm"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-typedef struct {
-  void          *data;
-  int            count;
-  int            capacity;
-  const __type_t *type;
-} DynamicArray;
-
-DynamicArray da_create(const __type_t *t) {
-  DynamicArray da = {NULL, 0, 0, t};
-  return da;
-}
-
-void da_destroy(DynamicArray *da) {
-  free(da->data);
-  da->data = NULL;
-  da->count = 0;
-  da->capacity = 0;
-}
-
-void da_push(DynamicArray *da, void *val) {
-  if (da->count >= da->capacity) {
-    int new_cap = da->capacity ? da->capacity * 2 : 4;
-    void *new = realloc(da->data, (size_t)new_cap * da->type->size);
-    da->data = new;
-    da->capacity = new_cap;
-  }
-  memcpy((char*)da->data + da->count * da->type->size, val, da->type->size);
-  da->count++;
-}
-
-void *da_get(DynamicArray *da, int i) {
-  return (char*)da->data + i * da->type->size;
-}
 
 int main() {
   /* array of ints */
   DynamicArray da = da_create(__type_of(int));
   printf("int array: size=%lu align=%lu kind=%d\n",
-         (unsigned long)da.type->size, (unsigned long)da.type->align, (int)da.type->kind);
+         (unsigned long)da.element_type.size,
+         (unsigned long)da.element_type.align,
+         (int)da.element_type.kind);
 
   int vals[] = {10, 20, 30, 40, 50};
   for (int i = 0; i < 5; i++)
-    da_push(&da, &vals[i]);
+    da_push(&da, opaque(vals[i]));
 
   printf("count=%d capacity=%d\n", da.count, da.capacity);
-  for (int i = 0; i < da.count; i++)
-    printf("  [%d] = %d\n", i, *(int*)da_get(&da, i));
+  for (int i = 0; i < da.count; i++) {
+    Opaque *op = da_get(&da, i);
+    printf("  [%d] = %d\n", i, *(int*)op.data);
+  }
 
   da_destroy(&da);
 
@@ -57,11 +25,13 @@ int main() {
   DynamicArray dad = da_create(__type_of(double));
   double dvals[] = {1.1, 2.2, 3.3};
   for (int i = 0; i < 3; i++)
-    da_push(&dad, &dvals[i]);
+    da_push(&dad, (Opaque){ &dvals[i], __type_of(double) });
 
   printf("\ndouble array: count=%d\n", dad.count);
-  for (int i = 0; i < dad.count; i++)
-    printf("  [%d] = %g\n", i, *(double*)da_get(&dad, i));
+  for (int i = 0; i < dad.count; i++) {
+    Opaque *op = da_get(&dad, i);
+    printf("  [%d] = %g\n", i, *(double*)op.data);
+  }
 
   da_destroy(&dad);
 
